4b4f2b9217428cd518923cbb630af4b1ea79e77f
Femi Oluwadamilola <oluwadamilola1902@gmail.com>
2021-11-29 21:20:15 +0100

    serve Static with express in myApp.js file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
+
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/views/index.html' )
 })
+
+app.use(express.static(__dirname + '/public/style.css'));
 
 app.listen()
 
