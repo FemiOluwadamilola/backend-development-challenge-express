093b5c6e6827573e889a495234a8ea1c0b1d1157
Femi Oluwadamilola <oluwadamilola1902@gmail.com>
2021-11-29 21:28:00 +0100

    modify the static assets in the myApp.js file

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 
@@ -6,7 +7,7 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + '/views/index.html' )
 })
 
-app.use(express.static(__dirname + '/public/style.css'));
+app.use(express.static(path.join(__dirname, '/public/style.css')));
 
 app.listen()
 
